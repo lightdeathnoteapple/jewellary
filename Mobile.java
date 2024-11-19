fc57433c90599eb2822be6da70977003b580356c
sann <sannemail.com>
2024-11-19 20:54:49 +0530

    update in master

---
+++ Mobile.java
@@ -0,0 +1,18 @@
+class Mobile{
+   String model;
+   int price;
+
+Mobile(int p, String m){
+      this.model=m;
+      this.model=p;
+ }
+public static void main (String[] args){
+      Mobile m1=new Mobile(25000, "vivo");
+      System.out.println(m1.model ="  "=m1.price);
+
+
+      Mobile m1=new Mobile(50000, "samsung");
+      System.out.println(m2.model ="  "=m2.price);
+
+}
+}
---
+++ car.java
@@ -0,0 +1,35 @@
+class Car {
+        void start () {
+            System.out.println("car started");
+         }
+        void changeGear()  {
+            Sytem.out.println("gear changed");
+         }
+        public static void main(String[] args) {
+           Car c=new Car ();
+           c.start();
+           c.changeGear();
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
